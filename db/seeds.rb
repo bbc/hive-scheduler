a5d97bff9dd9cfa9f054159cf1ec2c6b497020eb
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-07-21 09:59:19 +0100

    Changed DB seed to say 'iOS IPA' instead of 'iOS APK'

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Field.where(owner_type: "Target").delete_all
 # Key the target types by database ID so we maintain existing relations
 {
     1 => { name: "Android APK", icon: "android", requires_build: true, fields: {} },
-    2 => { name: "iOS APK", icon: "apple", requires_build: true, fields: {} },
+    2 => { name: "iOS IPA", icon: "apple", requires_build: true, fields: {} },
     3 => { name: "Mobile Browser", icon: "globe", requires_build: false, fields: { url: :string} },
     4 => { name: "TAL TV App", icon: "desktop", requires_build: false, fields: { application_url: :string, application_url_parameters: :string } },
     5 => { name: "Shell Script", icon: "file-text-o", requires_build: false, fields: {} }
