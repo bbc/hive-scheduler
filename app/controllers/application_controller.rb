05d896924cd27abe7ab62c3ede98bf6b5813bbec
Joe Haig <joe.haig@bbc.co.uk>
2015-07-28 11:38:05 +0100

    Display recent batch statuses in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   def generate_stats
     @counts  = Job.state_counts
     @version = Hive::Scheduler.const_get(:VERSION)
+    @projects = Project.all.sort { |a, b| a.latest_batch.updated_at <=> b.latest_batch.updated_at }[0, 10]
   end
 
   def current_user
