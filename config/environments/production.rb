1d029f785794bf152bd93d227bbee229f9a5cfd6
David Buckhurst <david.buckhurst@bbc.co.uk>
2015-10-14 21:43:40 +0100

    Asset pipeline fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ Hive::Scheduler::Application.configure do
   #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
