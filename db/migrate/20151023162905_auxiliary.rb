3a4cb50e1088f57af46f707fc1be28d75ae1a144
asim <asimk21@gmail.com>
2015-11-02 11:49:28 +0000

    Scheduler to use sqlite3 db

--- db/migrate/20151023162905_auxiliary.rb
+++ db/migrate/20151023162905_auxiliary.rb
@@ -1,6 +1,11 @@
 class Auxiliary < ActiveRecord::Migration
 	def self.up
-		execute "TRUNCATE schema_migrations;"
+		adapter = ActiveRecord::Base.connection.instance_values["config"][:adapter]
+		if adapter == "sqlite3"
+			execute "delete from schema_migrations;"
+		else
+			execute "TRUNCATE schema_migrations;"
+		end
 		execute "INSERT INTO schema_migrations VALUES ('20151002073630');"
 	end
 	def self.down
