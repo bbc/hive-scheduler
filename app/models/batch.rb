57bec02d7a3cd32cc840bb1b8fb7668e638e73b7
David Buckhurst <david.buckhurst@bbc.co.uk>
2016-02-09 18:02:22 +0000

    Cut down on the number of database calls

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -34,13 +34,16 @@ class Batch < ActiveRecord::Base
   end
 
   def state
-    ordered_job_states = [:running, :queued, :errored, :failed, :passed, :complete, :analyzing, :preparing, :retried, :cancelled]
-    job_states         = latest_jobs.collect { |job| job.status.to_sym }.uniq
-    job_states.delete(:reserved)
-    job_states = job_states.sort_by { |state| ordered_job_states.index(state) }
-    job_states.find do |state|
-      ordered_job_states.include?(state)
-    end || :invalid
+    if !@state_cache
+      ordered_job_states = [:running, :queued, :errored, :failed, :passed, :complete, :analyzing, :preparing, :retried, :cancelled]
+      job_states         = latest_jobs.collect { |job| job.status.to_sym }.uniq
+      job_states.delete(:reserved)
+      job_states = job_states.sort_by { |state| ordered_job_states.index(state) }
+      @state_cache = job_states.find do |state|
+        ordered_job_states.include?(state)
+      end || :invalid
+    end
+    @state_cache
   end
   
   def test_result_count_hash
@@ -51,24 +54,30 @@ class Batch < ActiveRecord::Base
     @hash
   end
   
+  def active_job_states( state, result )
+    if !@active_job_states
+      @active_job_states = jobs.active.group('jobs.state').group('jobs.result').count
+    end
+    @active_job_states[[state, result]] || 0
+  end
  
   #
   # These methods provide a summary of the job statuses
   #
   def jobs_queued
-    jobs.active.where(state: ['queued', 'pending']).count
+    active_job_states("queued", nil) + active_job_states("pending", nil)
   end
 
   def jobs_running
-    jobs.active.where(state: ['preparing', 'running', 'analyzing']).count
+    active_job_states( "preparing", nil ) + active_job_states( "running", nil ) + active_job_states( "analyzing", nil )
   end
 
   def jobs_passed
-    jobs.active.where(state: 'complete', result: ['passed']).count
+    active_job_states( "complete", "passed" )
   end
 
   def jobs_failed
-    jobs.active.where(result: ['failed']).count
+    active_job_states( "complete", "failed" )
   end
   
   def jobs_errored
--- app/models/concerns/batch/batch_scopes.rb
+++ app/models/concerns/batch/batch_scopes.rb
@@ -3,15 +3,19 @@ class Batch < ActiveRecord::Base
     extend ActiveSupport::Concern
 
     included do
+      
     end
 
     def latest_jobs
-      self.jobs.where.not(
+      if !@latest_jobs_cache
+        @latest_jobs_cache = self.jobs.where.not(
           id: Job
               .joins("INNER JOIN jobs AS linked_jobs ON jobs.id = linked_jobs.original_job_id")
               .joins(:job_group)  # avoid full table scan on inner query by constraining it to the relevant batch
               .where(job_groups: { batch_id: self.id })
-      )
+        )
+      end
+      @latest_jobs_cache
     end
   end
 end
